f3dcf2add0eb8d58fce7bcdf5a1b76638db01c7a
Sakshee Phade <sakshee1120@gmail.com>
2020-07-27 20:00:43 +0530

    Components Part 1

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-import Media from 'react-bootstrap/Media';
+import { Media } from 'reactstrap';
+//import Media from 'react-bootstrap/Media';
 
 class Menu extends Component {
 
@@ -45,11 +46,29 @@ class Menu extends Component {
         }
     }
 
-    
+//constructor ends
 
     render() {
 
-        const menu;
+        const menu = this.state.dishes.map( 
+            (dish) => {
+                return(
+                    <div key={dish.id} className="col-12 mt-5">
+                        <Media tag="li">
+                            <Media left middle>
+                                <Media object src={dish.image} alt={dish.name}/>
+                            </Media>
+                            <Media body className="ml-5">
+                                <Media heading>{dish.name}</Media>
+                                <p>{dish.description}</p>
+                            </Media>
+                        </Media>
+                    </div>
+                );
+            }
+         );
+
+//Map ends
 
         return (
             <div className = "container">
