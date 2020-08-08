59acbaa7987570a5d77f17ea7bc66ab8afbcc762
Sakshee Phade <sakshee1120@gmail.com>
2020-08-08 13:23:02 +0530

    Header footer

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,78 +1,97 @@
-import React, {Component} from 'react';
+import React from 'react';
 import { Card, CardBody, CardImg, CardImgOverlay, CardText, CardTitle } from 'reactstrap';
 import Media from 'react-bootstrap/Media';
 
-class Dishdetail extends Component {
+//during initial call, dish is empty because user hasnt clicked. Thats why the problem. Did not write if else.
+ function RenderComments({dish}) {
 
-    constructor(props) {
-        super(props);
-
-        this.state = {
-        }
+    console.log("Rendercomments called!");
+   
+    if(dish==null) {
+        return(
+        <div>
+            {console.log("Dish is null, as in components.")}
+        </div>
+        );
     }
-
-    renderComments(comments) {
-
-        if(comments!=null) {
-            return (
-                <div className="container">
-                    <div>
-                        <h4> Comments </h4>
-                    </div>
-                    <div>
-                    {
-                        comments.map(
-                                    comment => (
-                                        <ul type="none">
-                                        <li>{comment.comment}</li>
-                                        <li>--{comment.author},
-                                        {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))} </li>
-                                        </ul>
+    else{
+        console.log(dish.name)
+        let comments = dish.comments;
+            if(comments!=null) {
+                return (
+                    <div className="container">
+                        <div>
+                            <h4> Comments </h4>
+                        </div>
+                        <div>
+                        {
+                            comments.map(
+                                        comment => (
+                                            <ul type="none">
+                                            <li>{comment.comment}</li>
+                                            <li>--{comment.author},
+                                            {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))} </li>
+                                            </ul>
+                                        )
                                     )
-                                )
-                        }
+                            }
+                        </div>
                     </div>
-                </div>
-            )
+                );
+            }
+            else {
+                return(<div>
+                    {console.log("WTH is the problen!!")}
+                    </div>);
+            }
+
         }
+}
 
-    }
+    function RenderDish({dish}) {
 
-    render() {
-
-        //dish = this.props.dishes;
-        if(this.props.dish!=null) {
+        console.log("Renderdish called");
+        
+        if(dish!=null) {
             return (
-                <div className="container">
-                <div className="row">
-                    <div  className="col-12 col-md-5 m-1">
-                        <Card >
-                        <CardImg width="100%" src={this.props.dish.image} alt={this.props.dish.name}/>
-                        <CardBody>
-                            <CardTitle>{this.props.dish.name}</CardTitle>
-                            <CardText>{this.props.dish.description}</CardText>
-                        </CardBody>
-                        </Card>
-                    </div>
-                    <div className="col-12 col-md-5 m-1">
-                        <p>{this.renderComments(this.props.dish.comments)}</p>
-                    </div> 
+                <div>
+                    <Card >
+                    <CardImg width="100%" src={dish.image} alt={dish.name}/>
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                        {console.log(dish.name)}
+                    </CardBody>
+                    </Card>
                 </div>
-                </div>
-                
             );
         }
         else {
             return (
                 <div>
-
+                    {console.log("Dish is null!")}
                 </div>
             );
         }
-        
     }
 
-}
+    const Dishdetail = (props) => {
+
+        //dish = this.props.dishes;
+        return(
+            <div className="container">
+                <div className="row">
+                    <div  className="col-12 col-md-5 m-1">
+                        <RenderDish dish={props.dish} />
+                    </div>
+                    <div className="col-12 col-md-5 m-1">
+                        {console.log("Goddddd")}
+                        <RenderComments dish={props.dish} />
+                    </div> 
+                </div>
+            </div>
+        );
+    }
 
 export default Dishdetail;
 
@@ -80,4 +99,5 @@ export default Dishdetail;
    <li>--{comment.author}, 
                                         { new Intl.DateTimeFormat('en-US', {year: 'numeric', month: 'short', day: '2-digit'}).format( 
                                             new Date.parse(comment.date)) } </li>
-*/
+
+     */
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
+import Home from './HomeComponent';
 import Menu from './MenuComponent';
-//import './App.css';
 import { DISHES } from './shared/dishes';
 import Dishdetail from './DishdetailComponent';
-
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
 
@@ -26,13 +27,10 @@ class Main extends Component {
   render() {
     return (
       <div className="App">
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/"> Ristorante Con Fusion </NavbarBrand>
-          </div>
-        </Navbar>
+        <Header/>
         <Menu dishes={this.state.dishes} onClick = { (dishId) => { this.onDishselect(dishId) } }/>
         <Dishdetail dish={ this.state.dishes.filter( (dish) => dish.id === this.state.selectedDish )[0] } />
+        <Footer/>
       </div>
     );
   }
