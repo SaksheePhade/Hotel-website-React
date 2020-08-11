ddeb2536e695d40b453c3e45251fd6ec60afa5ba
Sakshee Phade <sakshee1120@gmail.com>
2020-08-11 09:25:56 +0530

    React router

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -13,23 +13,29 @@ class Main extends Component {
     super(props);
 
     this.state = {
-      dishes : DISHES,
-      selectedDish : null,
+      dishes : DISHES
     };
-
   }
 
-    onDishselect(dishId) {
-        this.setState( {selectedDish: dishId} )
+  
+
+  render() {
+    
+    const HomePage = () => {
+      return(
+        <Home/>  
+      );
     }
 
 
-  render() {
     return (
       <div className="App">
         <Header/>
-        <Menu dishes={this.state.dishes} onClick = { (dishId) => { this.onDishselect(dishId) } }/>
-        <Dishdetail dish={ this.state.dishes.filter( (dish) => dish.id === this.state.selectedDish )[0] } />
+        <Switch>
+          <Route path="/home" component={ HomePage }/>
+          <Route exact path="/menu" component={ () => <Menu dishes={this.state.dishes}  /> } />
+          <Redirect to="/home"/>
+        </Switch>
         <Footer/>
       </div>
     );
